e54d992217229f296505a042507608f3e0249cbc
volodymyr-hrytsenko <volodyk753@gmail.com>
2020-07-23 18:11:06 +0300

    changed getting data, parse data

--- index.js
+++ index.js
@@ -1,79 +1,94 @@
 let allText = '',
     arrData = null;
 
-function readTextFile(file) {
-    let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    let rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function () {
-        if (rawFile.readyState === 4) {
-            if (rawFile.status === 200 || rawFile.status == 0) {
-                allText = rawFile.responseText;
+function readFile(input) {
+    let file = input.files[0];
+    let reader = new FileReader();
 
-                // console.log(typeof(allText));
-                // console.log(allText);
-            }
-        }
+    reader.readAsText(file);
+
+    reader.onload = function () {
+        // console.log(reader.result);
+        allText = reader.result;
     };
-    rawFile.send(null);
+    reader.onerror = function () {
+        console.log(reader.error);
+    };
+
 }
 
-function CSVToArray(strData, strDelimiter) {
-    strDelimiter = (strDelimiter || ",");
-
-    // Create a regular expression to parse the CSV values.
-    let objPattern = new RegExp(
-        (
-            "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
-
-            "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
-
-            "([^\"\\" + strDelimiter + "\\r\\n]*))"
-        ),
-        "gi"
-    );
-
-    let arr = [
-        []
-    ];
-
-    let arrMatches = null;
-
-    while (arrMatches = objPattern.exec(strData)) {
-        let strMatchedDelimiter = arrMatches[1];
-
-        if (
-            strMatchedDelimiter.length &&
-            strMatchedDelimiter !== strDelimiter
-        ) {
-            arr.push([]);
-        }
-
-        let strMatchedValue;
-
-        if (arrMatches[2]) {
-            strMatchedValue = arrMatches[2].replace(
-                new RegExp("\"\"", "g"),
-                "\""
-            );
-        } else {
-            strMatchedValue = arrMatches[3];
-        }
-        arr[arr.length - 1].push(strMatchedValue);
-    }
-    return (arr);
+function CSVToArray(strData) {
+    const arr = strData.split('\r\n'),
+        result = arr.map(el => {
+            return el.split(',');
+        });
+    result.shift();
+    return result;
 }
 
-readTextFile("file://C:/programming/test_task/acme_worksheet.csv");
+function parseData(arrData) {
+    const data = {},
+        uniqueDate = {},
+        arrResult = [
+            []
+        ];
 
-arrData = CSVToArray(allText, ',');
+    arrData.forEach(element => {
+        if (!data.hasOwnProperty(element[0])) {
+            data[element[0]] = {};
+        }
+        data[element[0]][element[1]] = element[2];
+        uniqueDate[element[1]] = 1;
+    });
 
-console.log(arrData);
+    const getOwnPropertyNames = Object.getOwnPropertyNames(data);
+    const keys = Object.keys(uniqueDate);
+
+    getOwnPropertyNames.forEach((key, index) => {
+        arrResult[index] = [key];
+        keys.forEach(date => {
+            if (typeof data[key][date] !== 'undefined'){
+                arrResult[index].push(data[key][date]);
+            }else{
+                arrResult[index].push('-');
+            }
+        });
+    });
+
+    return arrResult;
+}
+
+function see() {
+    arrData = CSVToArray(allText);
+
+    // let test = parseData(arrData)
+
+    console.log(parseData(arrData));
+}
 
 
-var a = ['a', 1, 'a', 2, '1'];
-var unique = a.filter((value, index, self) => {
-    return self.indexOf(value) === index;
-});
 
-console.log(unique);
+
+
+
+
+
+
+
+
+
+// const rows = [
+//     ["name1", "city1", "some other info"],
+//     ["name2", "city2", "more info"]
+// ];
+
+// let csvContent = "data:text/csv;charset=utf-8," +
+//     rows.map(e => e.join(",")).join("\n");
+
+// var encodedUri = encodeURI(csvContent);
+// var link = document.createElement("a");
+// link.setAttribute("href", encodedUri);
+// link.setAttribute("download", "my_data.csv");
+// document.body.appendChild(link);
+
+// link.click();
